ad51648c780cf42d6994110ede0cae2825d3cb1f
darmijo15 <darmijo15@gmail.com>
2022-10-12 23:13:33 -0700

    define a template for the todo-item component

--- src/components/todo-item.ts
+++ src/components/todo-item.ts
@@ -2,9 +2,17 @@ import { FASTElement, customElement, html, css, attr } from "@microsoft/fast-ele
 
 @customElement({
     name: 'todo-item',
-    template: html<TodoItem>``,
+    template: html<TodoItem>`
+        <div class='item'>
+            <div class='input'>user input</div>
+                <div class='buttons'>
+                    <button>edit</button>
+                    <button>clear</button>
+                </div>
+        </div>
+    `,
     styles: css``,
 })
 export class TodoItem extends FASTElement {
-    
+
 }
