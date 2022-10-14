5da6b2d88d1eda860931ac7bec4a34af24ff81ac
darmijo15 <darmijo15@gmail.com>
2022-10-13 19:18:31 -0700

    use localStorage to store todo items the user has entered

--- src/script.ts
+++ src/script.ts
@@ -2,6 +2,14 @@ const form = document.querySelector('form');
 const input = <HTMLInputElement>document.querySelector('#item');
 const body = document.querySelector('body');
 
+let itemsArray = localStorage.getItem('items')
+    ? JSON.parse(localStorage.getItem('items')!)
+    : [];
+
+localStorage.setItem('items', JSON.stringify(itemsArray));
+
+const data = JSON.parse(localStorage.getItem('items')!);
+
 function addItem(text:string ) {
     const newItem = document.createElement('todo-item');
     newItem.innerHTML = text;
@@ -11,8 +19,14 @@ function addItem(text:string ) {
 form?.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    itemsArray.push(input.value);
+    localStorage.setItem('items', JSON.stringify(itemsArray));
     addItem(input.value);
     input.value = '';
 })
+
+data.forEach((item:string) => {
+    addItem(item);
+});
 
 export * from './components/todo-item';
